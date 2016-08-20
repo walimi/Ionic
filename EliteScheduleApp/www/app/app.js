da4ee0e3345481156709a13604056439c2295954
Wahid Alimi <wahid.alimi@gmail.com>
2016-08-20 18:40:59 -0400

    Navigation and Routing: Tabs - Setting up the initial Structure.

---
+++ EliteScheduleApp/www/app/app.js
@@ -0,0 +1,30 @@
+angular.module("eliteApp", ["ionic"])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+	$stateProvider
+
+	.state('home', {
+		url: "/home", 
+		templateUrl: "app/home/home.html"
+	});
+
+	// if none of the above states are matched, use this as fallback
+	$urlRouterProvider.otherwise('/home');
+});
